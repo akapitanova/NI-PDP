8a5048f67fbe2fba915423be301dcac885263315
kapitann <kapitann@fit.cvut.cz>
2023-03-05 17:22:53 +0100

    funkcni

--- du_1/main.cpp
+++ du_1/main.cpp
@@ -2,33 +2,32 @@
 #include <vector>
 #include <string>
 #include <map>
+#include<set>
 #include <fstream>
 #include <sstream>
+#include <limits>
 
 
 class Solution{
-public:
+private:
     int price;
     int n_vertices;
+    // TODO prepsat na vector
     std::map<int, int> X_vertices;
 
+public:
     Solution(int price, int n_vertices, std::map<int, int> X_vertices){
         this->price = price;
         this->n_vertices = n_vertices;
         this->X_vertices = X_vertices;
     }
 
-    Solution(Solution* old_solution){
-        price = old_solution->price;
-        n_vertices = old_solution->n_vertices;
-        X_vertices = old_solution->X_vertices;
-    }
-
-    Solution(Solution* old_solution, int new_vertex){
-        price = old_solution->price;
-        n_vertices = old_solution->n_vertices;
-        X_vertices = old_solution->X_vertices;
-        this->add_vertex(new_vertex);
+    Solution(int n){
+        price = 0;
+        n_vertices = 0;
+        for (int i = 0; i < n; i++){
+            X_vertices.insert({i, 0});
+        }
     }
 
     int get_price(){
@@ -39,29 +38,26 @@ public:
         return n_vertices;
     }
 
-    bool check_cond(int a){
-        if (a == n_vertices){
-            return true;
-        }
-        return false;
-    }
-
     std::map<int, int> get_X_vertices(){
         return X_vertices;
     }
-private:
-    void add_vertex(int vertex){
-        X_vertices[vertex] = 1;
-    }
+
 };
 
 class Graph{
 public:
+    // celkem pocet vrcholu
     int n;
+    // prumerny stupen vrcholu
     int k;
+    // pocet vrcholu mnoziny X
     int a;
+
     std::vector<std::vector<int>> edges_w;
-    Solution* bestSol;
+    Solution* best_sol;
+    int best_price;
+
+    int calls;
 
     Graph(int n, int k, int a, std::vector<std::vector<int>> edges_w){
         this->n = n;
@@ -69,6 +65,10 @@ public:
         this->a = a;
 
         this->edges_w = edges_w;
+
+        best_sol = nullptr;
+        best_price = std::numeric_limits<int>::max();
+        calls = 0;
     }
 
     int get_n(){
@@ -87,46 +87,135 @@ public:
         return edges_w[u_i][v_i];
     }
 
+    Solution* get_best_sol(){
+        return this->best_sol;
+    }
+
+    int get_best_price(){
+        return best_price;
+    }
+
+    int get_calls(){
+        return calls;
+    }
+
+    // BB-DFS pro hledani minimalniho hranoveho rezu
     void solve(Solution* solution){
-        // Průběžně počítáme váhu řezu pro již přiřazené uzly,
-        // pokud je tato váha větší než dosud nalezené minimum,
-        // tuto větev ukončíme.
-        if (this->bigger_than_min(solution)){
+        calls += 1;
+        int n_vertices = solution->get_n_vertices();
+
+        // ukončíme větev, pokud velikost menší podmnožiny uzlů překročí a
+        if (n_vertices > a){
             return;
         }
 
-        if (this->smaller_than_min(solution)){
-            bestSol = solution;
+        if (n_vertices == a){
+            // nalezeno prvni validni reseni
+            if(!best_sol){
+                best_sol = solution;
+                best_price = solution->get_price();
+            }
+
+            // cena menší než cena dosavadního nejlepšího řešení
+            // provede se aktualizace nejlepšího řešení
+            else if (this->smaller_than_min(solution)){
+                best_sol = solution;
+                best_price = solution->get_price();
+            }
+            return;
         }
 
-        int 
+        // volani rekurze
+        // TODO vector - jako pointer
+        std::map<int, int> X_vertices = solution->get_X_vertices();
+        std::set<int> free_vertices = get_free_vertices(X_vertices);
 
+        // TODO iterovat pres vector, continue pokud je obsah 1
+        for (auto it = free_vertices.begin(); it != free_vertices.end(); ++it){
+            // TODO podminka if obsah 1 -> continue
+            // TODO az zde delat deep copy
+            std::map<int, int> X_vertices_i = X_vertices;
+            X_vertices_i[*it] = 1;
+
+            int price_i = get_weights_sum(X_vertices_i);
+            if (price_i == -1){
+                continue;
+            }
+
+            int n_vertices_i = n_vertices + 1;
+
+            Solution solution_i = Solution(price_i, n_vertices_i, X_vertices_i);
+
+            // Větev ukončíme, i pokud v daném mezistavu pomocí dolního odhadu váhy zbývajícího řezu zjistíme,
+            // že z něj nelze žádným způsobem vytvořit přípustný koncový stav s menší vahou řezu,
+            // než je nejlepší dosud nalezené řešení
+            //if (!check_lower_cost_estimation(&solution_i)){
+            //    continue;
+            //}
+            solve(&solution_i);
+        }
     }
 
 
 private:
 
+    // return vertices in graph not already in X set (value in map is = 0)
+    std::set<int> get_free_vertices(std::map<int, int> X_vertices){
+        std::set<int> free_vertices;
+        for (int i = 0; i < this->n; i++){
+            if (X_vertices[i] == 0){
+                free_vertices.insert(i);
+            }
+        }
+        return free_vertices;
+    }
+
+    //TODO reference
     int get_weights_sum(std::map<int, int> X_vertices){
         int sum = 0;
 
         for (int row = 0; row < n; row++){
             for (int column = 0; column < n; column++){
                 if (X_vertices[row] == 1 and X_vertices[column] == 0){
+                    // Průběžně počítáme váhu řezu pro již přiřazené uzly,
+                    // pokud je tato váha větší než dosud nalezené minimum,
+                    // tuto větev ukončíme
                     sum += edges_w[row][column];
+                    if (sum > best_price){
+                        return -1;
+                    }
                 }
             }
         }
+
+        return sum;
     }
 
-    bool bigger_than_min(Solution* solution){
-        if (solution->get_price() > bestSol->get_price()){
-            return true;
+    bool check_lower_cost_estimation(Solution* solution){
+        if(!best_sol) return true;
+
+        int price = solution->get_price();
+        std::map<int, int> X_vertices = solution->get_X_vertices();
+        std::set<int> free_vertices = get_free_vertices(X_vertices);
+        int sum = price;
+        //TODO zase zbytecny set
+        for (auto it = free_vertices.begin(); it != free_vertices.end(); ++it){
+            std::map<int, int> X_vertices_i = X_vertices;
+            X_vertices_i[*it] = 1;
+            int new_price = get_weights_sum(X_vertices_i);
+
+            sum += new_price - price;
         }
-        return false;
+
+        if (sum > best_price){
+            return false;
+        }
+        return true;
     }
+
 
     bool smaller_than_min(Solution* solution){
-        if (solution->get_price() < bestSol->get_price()){
+        if (solution->get_price() < best_price){
             return true;
         }
         return false;
@@ -135,7 +224,7 @@ private:
 
 };
 
-Graph* load_data(std::string & dir_path, std::string & file_name, int a){
+Graph* load_data(std::string& dir_path, std::string& file_name, int a){
     if (a < 5)
         return nullptr;
 
@@ -183,13 +272,40 @@ Graph* load_data(std::string & dir_path, std::string & file_name, int a){
 }
 
 
+std::ostream & operator<< (std::ostream& os, Solution* solution)
+{
+    std::map<int, int> X_vertices = solution->get_X_vertices();
+    for (auto i : X_vertices){
+        if (i.second == 1){
+            os << i.first << ", ";
+        }
+    }
+    os << std::endl;
+
+    return os;
+}
 
 int main() {
 
     std::string dir_path = "/home/anna/skola/PDP/ni_pdp/graf_mro/";
-    std::string file_name = "graf_10_5.txt";
-    Graph* graph = load_data(dir_path, file_name, 5);
-    std::cout << graph->get_edge_w(9, 2);
 
+
+    std::string file_name = "graf_20_7.txt";
+    Graph* graph = load_data(dir_path, file_name, 7);
+    Solution first_solution = Solution(graph->get_n());
+    graph->solve(&first_solution);
+    std::cout << "best price " << graph->get_best_price() << std::endl;
+     std::cout << "total calls " << graph->get_calls() << std::endl;
+
+
+    /*
+    std::string file_name_1 = "graf_20_7.txt";
+    Graph* graph_1 = load_data(dir_path, file_name_1, 10);
+    Solution first_solution_1 = Solution(graph_1->get_n());
+    graph_1->solve(&first_solution_1);
+    //std::cout << graph->get_best_sol() << std::endl;
+    std::cout << "best price " << graph_1->get_best_price() << std::endl;
+    std::cout << "total calls " << graph_1->get_calls() << std::endl;
     return 0;
+     */
 }
